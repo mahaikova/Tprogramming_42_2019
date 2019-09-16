bb9584802fd36f29adcfdc3bbeee81637df934a9
jskonst <jskonst@yandex.ru>
2019-09-16 09:29:33 +0300

    Sample .net app

---
+++ CourseApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CourseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
