84dcf263ce3acad9877dce58a4ec24db78c725a7
Eugeny Konstantinov <eugeny.konstantinov@rp.ru>
2019-10-12 14:49:02 +0300

    First lab sample

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -10,5 +10,19 @@ namespace CourseApp.Tests
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void TestSummPositive()
+        {
+            var res = Program.Summ(2, 3);
+            Assert.Equal(5, res);
+        }
+
+        [Fact]
+        public void TestSummPositiveNegative()
+        {
+            var actualRes = Program.Summ(-2, 2);
+            Assert.Equal(0, actualRes);
+        }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,9 +4,42 @@ namespace CourseApp
 {
     public class Program
     {
+        public static double MyFunction(double a, double b, double x)
+        {
+            var y = (a * Math.Pow(x, 2)) + (b * x);
+            return y;
+        }
+
+        public static double[] TaskA(double a, double b, double xn, double xk, double dx)
+        {
+            return new double[0];
+        }
+
+        public static double[] TaskB(double a, double b, double[] x)
+        {
+            var y = new double[x.Length];
+            for (var i = 0; i < x.Length; i++)
+            {
+                y[i] = MyFunction(a, b, x[i]);
+            }
+
+            return y;
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            const double a = 2.2;
+            const double b = 3.8;
+            var resSingle = MyFunction(a, b, 4);
+            Console.WriteLine(resSingle);
+            var x = new double[] { 1, 2, 3, 4, 5 };
+            var taskBRes = TaskB(a, b, x);
+            foreach (var item in taskBRes)
+            {
+                Console.WriteLine($"y = {item}");
+            }
+
             Console.ReadLine();
         }
     }
