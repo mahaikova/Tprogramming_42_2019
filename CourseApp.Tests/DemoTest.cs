3ba806074c4e764e244817e31705c3ae1ae3a9bd
mahaikova <w039u428@mail.ru>
2019-11-09 14:56:15 +0400

    Tests are done

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,20 +5,42 @@ namespace CourseApp.Tests
 {
     public class DemoTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(0.1, 0.5, 0.15, 0)]
+        [InlineData(0.1, 0.5, 1.15, 0.0069)]
+        public void TestEquat(double a, double b, double x, double exp)
         {
-            Assert.True(true);
+            Assert.Equal(Program.Equat(a, b, x), exp, 4);
         }
 
-        [Theory]
-        [InlineData(0, 0, 0, 0)]
-        [InlineData(0, 2, 1, 2)]
-        [InlineData(1, 2, 1, 3)]
-        public void TestFunctionCalculationVal(double a, double b, double x, double exp)
+        [Fact]
+        public void TestTaskA()
         {
-            var res = Program.MyFunction(a, b, x);
-            Assert.Equal(exp, res, 3);
+            var res = Program.TaskA(0.1, 0.5, 2.15, 1.37, 0.25);
+            Assert.Equal(res, new double[0]);
+        }
+
+        [Fact]
+        public void TestTaskB()
+        {
+            double[] e = new double[0];
+            var res = Program.TaskB(0.1, 0.5, e);
+            Assert.Equal(res, new double[0]);
+        }
+
+        [Fact]
+        public void TestTaskBWork()
+        {
+            double[] mass = new double[5] { 0.2, 0.3, 0.44, 0.6, 0.56 };
+            var res = Program.TaskB(0.1, 0.5, mass);
+            int[] resMass = new int[mass.Length];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                resMass[i] = (int)Math.Floor(res[i]);
+            }
+
+            int[] exp = new int[5] { 0, 0, 0, 0, 0 };
+            Assert.Equal(resMass, exp);
         }
     }
 }
